57147bf6a938c76a25c6917aa253d3ab9b147bc1
29213 <50029213@iade.pt>
2020-01-14 12:30:34 +0000

    Alterei o nome da classe para ItemDoPedio

---
+++ Gestao De Inventario/src/pt/iade/gestaoInventario/models/ItemDoPedido.java
@@ -0,0 +1,74 @@
+package pt.iade.gestaoInventario.models;
+
+/**
+ * 
+ * Classe de dados.
+ * 
+ * @author Renato Pitta Simões
+ */
+public class ItemDoPedido {
+
+	private int idItemDeStock;
+	private int quantidade;
+	private double valor;
+	private Pedido pedido;
+	private Produto produto;
+
+	public ItemDoPedido() {
+
+	}
+	
+	public ItemDoPedido(int idItemDeStock, int quantidade, double valor) {
+		this.idItemDeStock = idItemDeStock;
+		this.quantidade = quantidade;
+		this.valor = valor;
+	}
+
+
+	public int getIdItemDeStock() {
+		return idItemDeStock;
+	}
+
+	public void setIdItemDeStock(int idItemDeStock) {
+		this.idItemDeStock = idItemDeStock;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Pedido getStock() {
+		return pedido;
+	}
+
+	public void setStock(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	@Override
+	public String toString() {
+		return produto + " -> " + quantidade + " -> Preço: "+ 
+	String.format("%.2f€", getValor());
+	}
+
+}
